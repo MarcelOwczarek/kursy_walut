0fb7f8e991075d814de068a2ed5bc1acadcfc3f6
Marcel <owczarek2000@gmail.com>
2023-03-21 00:46:20 +0100

    Add appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,17 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'Kursy walut',
+              style: GoogleFonts.raleway(letterSpacing: 2),
+            ),
+          ),
+          backgroundColor: Colors.grey[900],
+        ),
+      ),
     );
   }
 }
