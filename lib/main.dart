634a446ce52f58df686e047bdf40e6d7e0ad770c
Marcel <owczarek2000@gmail.com>
2023-03-21 03:00:01 +0100

    Add CurrencyContainer

--- lib/main.dart
+++ lib/main.dart
@@ -75,20 +75,92 @@ class _MyAppState extends State<MyApp> {
             child: Builder(
               builder: (context) {
                 if (currentIndex == 0) {
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [Text('1')],
-                  );
+                  return const CurrencyContainer();
                 }
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [Text('2')],
-                );
+                return const CurrencyContainer();
               },
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class CurrencyContainer extends StatelessWidget {
+  const CurrencyContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Container(
+            height: 505,
+            width: 340,
+            padding: const EdgeInsets.all(0),
+            margin: const EdgeInsets.all(0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: const LinearGradient(
+                colors: [
+                  Color(0xFF292E49),
+                  Color(0xFF536976),
+                ],
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+              ),
+            ),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Text(
+                      'Data aktualnego kursu',
+                      style: GoogleFonts.poppins(
+                          color: Colors.white, fontSize: 15),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '1 EUR - ',
+                      style: GoogleFonts.poppins(
+                          color: Colors.white, fontSize: 23),
+                    ),
+                    Text(
+                      '4,71 zł',
+                      style: GoogleFonts.poppins(
+                          color: Colors.white, fontSize: 25),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                Text('Wykres ostatnie 30 dni ',
+                    style:
+                        GoogleFonts.poppins(color: Colors.white, fontSize: 20)),
+                const Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Divider(
+                    thickness: 3,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
